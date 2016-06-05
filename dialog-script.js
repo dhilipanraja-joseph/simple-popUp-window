98ea7a50ab036300a3d863b41896f30f5f3c6a81
Dhilipanraja Joseph <dlipnraj@gmail.com>
2016-06-05 08:56:26 -0500

    Add files via upload

---
+++ dialog-script.js
@@ -0,0 +1,6 @@
+function popDialog(){
+	document.getElementsByClassName("dialog-background").style.visibility="visible";
+}
+function closeDialog(){
+	document.getElementsByClassName('dialog-background').style.visibility = "hidden";
+}
